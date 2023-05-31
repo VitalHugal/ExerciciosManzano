7b512c9413a37241636d4f354d94147ab322d42c
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2023-05-30 21:34:17 -0300

    Manzano1c.java

--- Manzano1c.java
+++ Manzano1c.java
@@ -1,10 +1,22 @@
+
+//c)Elaborar um programa que apresente no final o somatório dos valores pares existentes na faixa de 1 até 500. 
+
 package manzano;
 
 public class Manzano1c {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int valor = 1;
+		int soma = 0;
+
+		while (valor < 501) {
+			valor++;
+			if (valor % 2 == 0) {
+				soma += valor;
+			}
+		}
+		System.out.println(soma);
 	}
 
 }
