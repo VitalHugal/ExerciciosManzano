2462b98cef8882dd4cb173709e577ba6faf20f69
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2023-05-30 21:35:37 -0300

    Manzano1e.java

--- Manzano1e.java
+++ Manzano1e.java
@@ -1,10 +1,23 @@
+//e) Apresentar os resultados das potências de 3, variando do expoente 0 até o expoente 15. Deve ser considerado  que  qualquer  
+//número  elevado  a  zero  é  1,  e  elevado  a  1  é  ele  próprio.  Observe  que neste exercício não pode ser 
+//utilizado o operador de exponenciação do portuguol (^).
+
 package manzano;
 
 public class Manzano1e {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double base = 3;
+		double expoente = 0;
+		double resultado = 0;
+		
+		while(expoente<16) {
+			resultado = Math.pow(base, expoente);
+			expoente++;
+			System.out.println(resultado);
+		}
+		
 	}
 
 }
