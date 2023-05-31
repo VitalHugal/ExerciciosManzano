7a61a8ca9e81693caaa7e3a82091f78de4c22e76
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2023-05-30 21:34:56 -0300

    Update Manzano1d.java

--- Manzano1d.java
+++ Manzano1d.java
@@ -1,9 +1,21 @@
+//d)   Apresentar todos os valores numéricos inteiros ímpares situados na faixa de 0 a 20. Para verificar se o número 
+//é ímpar, efetuar dentro da malha a verificação lógica desta condição com a instrução se, perguntando se o número é ímpar; 
+//sendo, mostre-o; não sendo, passe para o próximo passo. 
+
 package manzano;
 
 public class Manzano1d {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int valor = 0;
+
+		while (valor < 20) {
+			valor++;
+			if (valor % 2 == 1) {
+				System.out.println(valor);
+			}
+		}
 
 	}
 
